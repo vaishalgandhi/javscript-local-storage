4b1ef259e5b21eb39fe81c32f86f01bc2c1b293e
Vaishal Gandhi <vaishal48@gmail.com>
2020-01-28 23:00:24 -0500

    step 4 commit

---
+++ Vaishal/script - 3.js
@@ -0,0 +1,95 @@
+if (typeof(Storage) == "undefined") {
+	document.body.innerHTML = "Your browser does not support local storage";
+}
+
+var rowId = 0;
+
+if(localStorage.getItem("table-records") === null) {
+	var entries = [
+		{id: 1, "first": "Vaishal", "last": "Gandhi", "age": 21},
+		{id: 2, "first": "Rahul", "last": "Nagpal", "age": 24},
+		{id: 3, "first": "Hasmukh", "last": "Solanki", "age": 25},
+	];
+
+	localStorage.setItem("table-records", JSON.stringify(entries));
+
+	rowId = 3;
+} else {
+	var entries = JSON.parse(localStorage.getItem("table-records"));
+
+	entries.map(function(item) {
+		if(rowId < item.id) {
+			rowId = item.id
+		}
+	});
+}
+
+
+function fetchElement(id) {
+	return document.getElementById(id);
+}
+
+function addRecord() {
+	var first = fetchElement("first").value,
+		last = fetchElement("last").value,
+		age = fetchElement("age").value;
+
+	if(first === "" || first === " ") {
+		alert("Please enter first name");
+		return;
+	}
+
+	if(last === "" || last === " ") {
+		alert("Please enter last name");
+		return;
+	}
+
+	if(age === "" || age === " ") {
+		alert("Please enter age name");
+		return;
+	}
+
+	if (age <= 0 || age >=100) {
+		alert("Please provide valid age");
+		return;
+	}
+
+	rowId++;
+
+	var text = "<tr id=entry-"+rowId+"><td><button class=\"btn-danger\" onclick=\"deleteRecord("+rowId+")\">Delete</button></td><td>"+ first +"</td><td>"+ last +"</td><td>"+ age +"</td></tr>";
+
+	fetchElement("table-body").innerHTML += text;
+
+	var entries = JSON.parse(localStorage.getItem("table-records"));
+	entries.push({id: rowId, "first": first, "last": last, "age": age});
+	localStorage.setItem("table-records", JSON.stringify(entries));
+	
+	fetchElement("first").value = "";
+	fetchElement("last").value = "";
+	fetchElement("age").value = "";
+
+	return true;
+}
+
+function deleteRecord(id) {
+	document.getElementById("entry-"+id).remove();
+
+	var entries = JSON.parse(localStorage.getItem("table-records"));
+	
+	entries = entries.filter(function( object ) {
+		return object.id !== id;
+	});
+
+	localStorage.setItem("table-records", JSON.stringify(entries));
+}
+
+function refreshTable() {
+	var text = "",
+		entries = JSON.parse(localStorage.getItem("table-records"));
+	
+	entries.forEach(item => {
+		text += "<tr id=entry-"+ item.id +"><td><button class=\"btn-danger\" onclick=\"deleteRecord("+item.id+")\">Delete</button></td><td>"+ item.first +"</td><td>"+ item.last +"</td><td>"+ item.age +"</td></tr>";
+	});
+
+	fetchElement("table-body").innerHTML = text;
+}
