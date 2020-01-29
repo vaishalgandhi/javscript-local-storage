c0b79c851c990ca2fdd096e2418b25e853a9be0d
Rahul <59620123+nagpalr@users.noreply.github.com>
2020-01-29 18:51:32 -0500

    Implement Local Storage using JS

---
+++ Rahul/script2.js
@@ -0,0 +1,56 @@
+﻿var mydata = ["Sagar","Abhishek","Shubh"];
+
+var id=0;
+
+function onEnterPressed(event){
+    if(event.key !== "Enter"){
+        return;
+    }
+
+    addEntry();
+}
+
+function addEntry() {
+    snippet = `
+    <tr id="${id}">
+        <td onClick="removeEntry(${id})"}>❌</td>
+        <td>${getName()}</td>
+    </tr>
+    `
+    id +=1;
+    setName("");
+
+    document.getElementById("tbody").innerHTML += snippet;
+}
+
+function removeEntry(id) {
+    var removeItem = (document.getElementById(id).childNodes[3].innerText);
+	document.getElementById(id).remove();
+	
+	var n=mydata.indexOf(removeItem);
+    mydata.splice(n,1);
+}
+
+function refreshTable(){
+	var html = "";
+
+	mydata.forEach(item => {
+		html += `
+		<tr id="${id}">
+			<td onClick="removeEntry(${id})"}>❌</td>
+			<td>${item}</td>
+		</tr>
+		`
+		id++;
+	});
+
+	document.getElementById("tbody").innerHTML = html;
+}
+
+function getName() {
+    return document.getElementById("name").value;
+}
+
+function setName(name) {
+    return document.getElementById("name").value=name;
+}
